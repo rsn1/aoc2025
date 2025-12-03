0d4c8e9e323275abfeed420560dcf09709fa8714
rsn1 <jonisris@hotmail.com>
2025-12-03 18:57:51 +0100

    day3

--- day3/day3p2.py
+++ day3/day3p2.py
@@ -1,13 +1,30 @@
-with open("test.txt") as f:
+with open("input.txt") as f:
     data = f.read().split('\n')
 
-tot = 0
+def get_first_highest_number(row):
+    biggest_digit = max(row)
+    index = row.index(biggest_digit)
+    return biggest_digit, index
 
 DIGITS=12
+tot = 0
+
 for row in data:
-    rlen = len(row)
-    best = ""
-    for i in range(rlen-1,-1,-1):
-        best = row[i] + best
-    
-    print(best[:DIGITS])
+    output = ""
+    rowcpy = row[:]
+    for i in range(DIGITS):
+        #first iteration, remove last 11 elements, second iteration remove last 10, etc
+        elements_to_remove = DIGITS-1-i
+        if elements_to_remove != 0:
+            cur = rowcpy[:-elements_to_remove]
+        else:
+            cur = rowcpy
+        #pick first highest number
+        val, idx = get_first_highest_number(cur)
+        output = output + val
+        #consider rest of array after first highest number
+        rowcpy = rowcpy[idx+1:]
+    tot += int(output)
+
+print(tot)
+        
