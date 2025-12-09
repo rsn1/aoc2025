688602566091876f696cda3c4b66000011dd369a
rsn1 <jonisris@hotmail.com>
2025-12-09 18:36:59 +0100

    day9

--- day9/day9p2.py
+++ day9/day9p2.py
@@ -1,28 +1,26 @@
-from shapely import Polygon, intersection, plotting
+from shapely import Polygon
 from shapely.geometry import box
+import matplotlib.pyplot as plt
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     data =  [tuple(map(int,x.split(','))) for x in f.read().splitlines()]
 
-#swapped_data = [(y,x) for x,y in data]
 polygon = Polygon(data)
-print(polygon)
-            
-exit()
-#print(polygon)
-#x[0] is col
-#x[1] is row
 biggest = -1
-for x in swapped_data:
-    for y in swapped_data[1:]:
-        rect = box(min(x[1],y[1]),min(x[0],y[0]), max(x[1], y[1]),  max(x[0],y[0]))
+for x in data:
+    for y in data[1:]:
+        rect = box(min(x[0],y[0]), min(x[1],y[1]), max(x[0],y[0]), max(x[1], y[1]))
         if not polygon.covers(rect):
             #polygon must fully contain rectangle
             continue
-        inter = intersection(polygon,rect)
-        if inter.area > biggest:
-            biggest = inter.area
-            biggestshape = inter
+        area = (abs(x[0]-y[0])+1) * (abs(x[1]-y[1])+1)
+        if area > biggest:
+            biggest = area
+            biggestshape = rect
 
 print(biggest)
-print(biggestshape)
+#x1, y1 = polygon.exterior.xy
+#x2, y2 = biggestshape.exterior.xy
+#plt.plot(x1,y1)
+#plt.plot(x2,y2)
+#plt.show()
